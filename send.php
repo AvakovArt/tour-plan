fed439bfe81859f995461ca278b70b725aab90a2
AvakovArt <AA.UC.tshk.18@gmail.com>
2020-08-01 23:18:25 +0500

    create: phpmailer

---
+++ send.php
@@ -0,0 +1,71 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$pochta = $_POST['pochta'];
+
+// Формирование самого письма
+$title = "Заголовок письма";
+$body = "
+<h2>Новое обращение Best Tour plan</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+";
+$titleEmail = "Заголовок письма";
+$bodyEmail = "
+<h2>Письмо пользователя</h2>
+<b>Почта:</b> $pochta<br><br>
+";
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'AA.UC.tshk.18@gmail.com'; // Логин на почте
+    $mail->Password   = ''; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('AA.UC.tshk.18@gmail.com', 'Михаил Аваков'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('michaelsh1p@mail.ru');  
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Subject = $titleEmail;
+    $mail->Body = $body;
+    $mail->Body = $bodyEmail;
+    
+
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+if ($pochta) {
+  header('Location: newsletter.html');
+} else {
+  header('Location: thankyou.html');
+}
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+// header('Location: thankyou.html');
+?>
