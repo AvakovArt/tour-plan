18a4bb2bdeb1fe70e12d11955f97a1aed6a00ea2
AvakovArt <AA.UC.tshk.18@gmail.com>
2020-08-04 20:31:11 +0500

    fixed: validate

--- js/main.js
+++ js/main.js
@@ -46,29 +46,29 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
-  // Обработка форм
-  $(".form").each(function () {
-    $(this).validate({
-      errorClass: "invalid",
-      messages: {
-        name: {
-          required: "Укажите имя",
-          minlength: "Имя должно быть не короче 2 букв",
-        },
-        email: {
-          required: "We need your email address to contact you",
-          email: "Your email address must be in the format of name@domain.com",
-        },
-        phone: {
-          required: "Телефон обязателен",
-        },
-      },
-    });
-  });
   $(document).on("keydown", function (e) {
     if (e.keyCode == 27) var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
+  });
+});
+// Обработка форм
+$(".form").each(function () {
+  $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Укажите имя",
+        minlength: "Имя должно быть не короче 2 букв",
+      },
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com",
+      },
+      phone: {
+        required: "Телефон обязателен",
+      },
+    },
   });
 });
